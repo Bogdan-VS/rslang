81fdc970ee8f67ee288d6123006f67d542fd6d97
Bogdan Samonov <bogdansamonov@hmail.com>
2022-02-02 23:03:16 +0300

    init: init project

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+console.log(`${mode} mode`);
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+    assetModuleFilename: 'assets/images/[hash][name][ext]',
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/assets',
+          to: 'assets',
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+};
