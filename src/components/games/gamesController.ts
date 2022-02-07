c9273ac51160f94bb2f6118314d73428173caa26
geekedger <geekedger@gmail.com>
2022-02-07 23:48:45 +0300

    feat: add games controller

---
+++ src/components/games/gamesController.ts
@@ -0,0 +1,20 @@
+import GamesView from "./gamesVIew";
+
+export default class GamesController {
+
+  gamesView: GamesView;
+
+  constructor() {
+    this.gamesView = new GamesView();
+  }
+
+  activate(): void {
+    this.gamesView.renderPage();
+    console.log('GamesController activated')
+  }
+
+  deactivate(): void {
+    console.log('GamesController deactivated')
+  }
+
+}
---
+++ src/components/games/gamesVIew.ts
@@ -0,0 +1,71 @@
+
+export default class GamesView {
+  main: HTMLElement;
+
+  constructor() {
+    this.main = document.getElementById('main') as HTMLElement;
+  }
+
+  renderPage() {
+    const content = `
+    <section class="section games-section">
+    <div class="games-section__games-container">
+      <div class="games-section__game">
+        <div class="games-section__game__content">
+          <div class="games-section__game__content-top content-item">
+            <div class="content-item__logo">
+              <div class="content-item__logo-container">
+                <img alt="sprint" src="" class="content-item__logo__img" />
+              </div>
+            </div>
+            <div class="content-item__name">
+              <p class="content-item__name__text">Sprint</p>
+            </div>
+          </div>
+          <div class="games-section__game__content-middle content-item">
+            <p class="content-item__desc">
+              Проверьте, сколько очков вы можете получить за одну минуту, делая
+              обоснованные предположения о том, правильно переведено слово или нет
+            </p>
+          </div>
+          <div class="games-section__game__content-bottom content-item">
+            <button class="button content-item__button" type="button">
+              <span class="content-item__button__label">Играть</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="games-section__game">
+        <div class="games-section__game__content">
+          <div class="games-section__game__content-top content-item">
+            <div class="content-item__logo">
+              <div class="content-item__logo-container">
+                <img alt="audiocall" src="" class="content-item__logo__img" />
+              </div>
+            </div>
+            <div class="content-item__name">
+              <p class="content-item__name__text">Audiocall</p>
+            </div>
+          </div>
+          <div class="games-section__game__content-middle content-item">
+            <p class="content-item__desc">
+              Проверьте насколько вы внимательный слушатель, пытаясь подобрать
+              правильный перевод после услышанного слова.
+            </p>
+          </div>
+          <div class="games-section__game__content-bottom content-item">
+            <button
+              class="button content-item__button"
+              type="button"
+            >
+              <span class="content-item__button__label">play</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>`
+ 
+  this.main.innerHTML = content;    
+  }
+}
